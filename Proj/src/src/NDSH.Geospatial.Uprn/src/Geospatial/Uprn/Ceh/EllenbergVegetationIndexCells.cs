a804e65dff935efe6f9298043cc1ece2237816fd
gisvlasta <vasilis.vlastaras@gmail.com>
2025-08-14 08:54:05 +0100

    chore: added NDSH.Geospatial.Uprn project

---
+++ Proj/src/src/NDSH.Geospatial.Uprn/src/Geospatial/Uprn/Ceh/EllenbergVegetationIndexCells.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDSH.Geospatial.Uprn.Ceh {
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public class EllenbergVegetationIndexCells {
+
+  }
+
+}
---
+++ Proj/src/src/NDSH.Geospatial.Uprn/src/Geospatial/Uprn/Defra/ModelledBackgroundPollutionCells.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDSH.Geospatial.Uprn.Defra {
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public class ModelledBackgroundPollutionCells {
+
+  }
+
+}
