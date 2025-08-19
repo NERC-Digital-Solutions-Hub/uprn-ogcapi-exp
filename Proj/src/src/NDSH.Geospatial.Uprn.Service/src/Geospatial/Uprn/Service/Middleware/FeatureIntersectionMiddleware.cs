1311b2f4cb29091a0438940b71440d56aaf9fe0b
Sandor Magyari <magyari.sandor.istvan@gmail.com>
2025-08-19 17:10:39 +0100

    refactor: incomplete spatial intersection middleware

---
+++ Proj/src/src/NDSH.Geospatial.Uprn.Service/src/Geospatial/Uprn/Service/Middleware/FeatureIntersectionMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using OgcApi.Net.Controllers;
+
+namespace NDSH.Geospatial.Uprn.Service.Middleware {
+  public class FeatureIntersectionMiddleware {
+
+    private readonly RequestDelegate _next;
+    private readonly CollectionsController _controller;
+
+    public FeatureIntersectionMiddleware(RequestDelegate next, CollectionsController controller) {
+      _next = next;
+      _controller = controller;
+    }
+
+    public async Task InvokeAsync(HttpContext context, CollectionsController collectionsController) {
+
+      if (!context.Request.Path.Value.Contains("/items")) {
+        await _next(context);
+        return;
+      }
+
+      if (!context.Request.Query.ContainsKey("sourceSelector") && !context.Request.Query.ContainsKey("selectorIds")) {
+        await _next(context);
+        return;
+      }
+
+      var selectorSource = context.Request.Query["selectorSource"].ToString();
+      var selectorIds = context.Request.Query["selectorIds"].ToString().Split(',').Select(int.Parse).ToList();
+
+      var selectorResult = await collectionsController.GetItems(selectorSource)  // TODO: populate from context.Request.Query
+    }
+  }
+}
---
+++ Proj/src/src/NDSH.Geospatial.Uprn.Service/src/Geospatial/Uprn/Service/Middleware/SwaggerJsonModifierMiddleware.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Services;
+using OgcApi.Net.OpenApi;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NDSH.Geospatial.Uprn.Service.Middleware {
+  /// <summary>
+  /// Middleware to intercept the Swagger JSON generation, add 'fields' query parameter to /items endpoints,
+  /// and serve a cached modified JSON.
+  /// </summary>
+  public class SwaggerJsonModifierMiddleware {
+    private readonly RequestDelegate _next;
+    private readonly IOpenApiGenerator _generator;
+    private readonly string _swaggerPath;
+    private string? _cachedJson;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SwaggerJsonModifierMiddleware"/> class.
+    /// </summary>
+    /// <param name="next">The next middleware in the pipeline.</param>
+    /// <param name="generator">The generator creating the swagger.json</param>
+    /// <param name="swaggerPath">The path the json is served through.</param>
+    public SwaggerJsonModifierMiddleware(RequestDelegate next, IOpenApiGenerator generator, string swaggerPath) {
+      _next = next;
+      _generator = generator;
+      _swaggerPath = swaggerPath;
+    }
+
+    /// <summary>
+    /// Intercepts the HTTP request to serve a modified Swagger JSON with 'fields' parameter added to /items endpoints.
+    /// </summary>
+    /// <param name="context">Contains metadata of the request.</param>
+    /// <returns></returns>
+    public async Task InvokeAsync(HttpContext context) {
+      if (!context.Request.Path.Value.Equals(_swaggerPath, StringComparison.OrdinalIgnoreCase)) {
+        await _next(context);
+        return;
+      }
+
+      if (_cachedJson == null) {
+        var baseUri = new Uri($"{context.Request.Scheme}://{context.Request.Host}/api/ogc");
+        var doc = _generator.GetDocument(baseUri);
+
+        // Add 'fields' query parameter to /items and /items/{id} paths
+        foreach (var pathKvp in doc.Paths.Where(p => p.Key.Contains("/items"))) {
+          var pathItem = pathKvp.Value;
+          foreach (var op in pathItem.Operations.Values) {
+            op.Parameters ??= new List<OpenApiParameter>();
+            if (!op.Parameters.Any(p => p.Name == "fields")) {
+              op.Parameters.Add(new OpenApiParameter {
+                Name = "fields",
+                In = ParameterLocation.Query,
+                Description = "Comma-separated list of properties to include in the response",
+                Required = false
+              });
+            }
+          }
+        }
+
+        _cachedJson = doc.SerializeAsJson(Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0);
+      }
+
+      context.Response.ContentType = "application/json";
+      await context.Response.WriteAsync(_cachedJson);
+    }
+  }
+}
